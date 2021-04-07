ccf437aecc16a0e74a4cee5995afd8c8f9eca7c5
Lucius Fowler <luciusfow@gmail.com>
2021-04-06 22:50:06 -0700

    commit tree ca97778080d561c79ad54c35d86a714be7589941

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  
+//   if (distance <= 400) {
+//     return 'This one is on me!'
+  
+//   } else if (distance > 2000) {
+//     return 'I will gladly take your thirty bucks.'
+  
+//   } else if (distance >= 2501) {
+//     return 'No can do.'
+//   }
+// }
+if (distance >= 2501) {
+  return 'No can do.'
+} else if (distance >= 2000) {
+  return 'I will gladly take your thirty bucks.'
+}else if (distance <= 400) {
+  return 'This one is on me!'
+}
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.' // works with strick or loose equaility operator === or ==
+ 
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(generous){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  // if (generous = 'Thank you so much.'){
+  //   switch(generous){ 
+  //     case 'not as generous' === 'Thank you.'
+  //     default 'Bye.'
+  //   }
+  // }
+  switch (generous) {
+    case "generous" :
+    return 'Thank you so much.';
+    break;
+    case "not as generous" : 
+    return 'Thank you.';
+    break;
+    case(generous) : 
+    return 'Bye.';
+  }
 }
